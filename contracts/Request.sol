ed5c190b63752a4d2946ba239af533bfc9a7aadb
sjm150 <sjm150@snu.ac.kr>
2022-05-26 12:23:48 +0900

    fix requestMint, requestBurn, read

--- contracts/Request.sol
+++ contracts/Request.sol
@@ -12,42 +12,49 @@ contract RequestManager {
         bool accepted;
     }
 
-    Request[] private requests;
-    uint256 private nonce;
-    uint256 private bookmark;
+    Request[] requests;
+    //uint256 nonce;
+    uint256 bookmark;
 
-    address private token;
+    address token;
 
     constructor(address _token) {
         token = _token;
-        nonce = 0;
+        //nonce = 0;
         bookmark = 0;
     }
 
-    function requestMint(string memory _gameID, uint256 _amount) public returns (uint256 requestID) {
-        requestID = nonce++;
-        Request storage request = requests[requestID];
+    function requestMint(string memory _gameID, uint256 _amount) public returns (uint256) {
+        Request memory request;
         request.owner = msg.sender;
         request.gameID = _gameID;
         request.amount = _amount;
         request.mint = true;
         request.accepted = false;
+
+        requests.push(request);
+        return requests.length;
     }
 
-    function requestBurn(string memory _gameID, uint256 _amount) public returns (uint256 requestID) {
-        requestID = nonce++;
-        Request storage request = requests[requestID];
+    function requestBurn(string memory _gameID, uint256 _amount) public returns (uint256) {
+        Request memory request;
         request.owner = msg.sender;
         request.gameID = _gameID;
         request.amount = _amount;
         request.mint = false;
         request.accepted = false;
+
+        requests.push(request);
+        return requests.length;
     }
 
-    function read() public returns (uint256, string[] memory, uint256[] memory) {
-        uint256 cnt = nonce - bookmark;
+    function read() public returns (uint256, string[] memory, uint256[] memory, bool[] memory) {
+        uint256 len = requests.length;
+        uint256 cnt = len - bookmark;
+        require(cnt > 0, "RequestManager: Nothing to read");
         string[] memory gameID_ = new string[](cnt);
         uint256[] memory amount_ = new uint256[](cnt);
+        bool[] memory mint_ = new bool[](cnt);
 
         uint256 first = bookmark;
         for (uint i = 0; i < cnt; i++) {
@@ -56,8 +63,8 @@ contract RequestManager {
             amount_[i] = request.amount;
         }
 
-        bookmark = nonce;
-        return (first, gameID_, amount_);
+        bookmark = len;
+        return (first, gameID_, amount_, mint_);
     }
 
     function accept(uint256[] memory requestIDs) public returns (bool[] memory) {
